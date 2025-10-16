bf2c8dfbee01a15c71b10cb29a00d9337d3814d2
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-16 14:15:58 -0300

    Update AlunoRepository.java

--- repository/AlunoRepository.java
+++ repository/AlunoRepository.java
@@ -1,1 +1,10 @@
+package com.academia.repository;
 
+import com.academia.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    List<Aluno> findByBairro(String bairro);
+    List<Aluno> findByDataNascimentoBefore(LocalDate data);
+}
