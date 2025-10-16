b456331084c3b079dd4214ddad8455342661bf39
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-16 14:16:15 -0300

    Update AvaliacaoFisicaRepository.java

--- repository/AvaliacaoFisicaRepository.java
+++ repository/AvaliacaoFisicaRepository.java
@@ -1,1 +1,9 @@
+package com.academia.repository;
 
+import com.academia.model.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
+    List<AvaliacaoFisica> findByAlunoId(Long alunoId);
+}
