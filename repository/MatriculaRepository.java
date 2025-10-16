bb1259b1958c29d4567e2e212a31e6661f2267e0
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-16 14:16:37 -0300

    Update MatriculaRepository.java

--- repository/MatriculaRepository.java
+++ repository/MatriculaRepository.java
@@ -1,1 +1,8 @@
+package com.academia.repository;
 
+import com.academia.model.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+    Matricula findByAlunoId(Long alunoId);
+}
