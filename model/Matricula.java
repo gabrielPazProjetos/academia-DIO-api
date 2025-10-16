4a634a5bf9427976d9ab86ce8ae449a7512a61be
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-16 14:15:31 -0300

    Update Matricula.java

--- model/Matricula.java
+++ model/Matricula.java
@@ -1,1 +1,36 @@
+package com.academia.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @NotNull(message = "Data da matrícula é obrigatória.")
+    private LocalDateTime dataDaMatricula;
+
+    @NotEmpty(message = "Plano é obrigatório.")
+    private String plano;
+
+    @NotEmpty(message = "Status é obrigatório.")
+    private String status;
+
+    @Size(max = 300, message = "Observações devem ter no máximo 300 caracteres.")
+    private String observacoes;
+}
