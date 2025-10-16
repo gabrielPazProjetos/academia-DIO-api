1cc3445ac56af618a76b7d103cebfaf29f79d916
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-16 14:26:13 -0300

    Create AcademiaApiApplication.java

---
+++ AcademiaApiApplication.java
@@ -0,0 +1,11 @@
+package com.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiaApiApplication.class, args);
+    }
+}
